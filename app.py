593895b5938a9060f155a8108e71df55763f5e55
Mayank Jha <mayankxjha@gmail.com>
2023-04-11 20:22:02 +0530

    final touches

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def index():
         y_pro_non_phishing = gbc.predict_proba(x)[0,1]
         # if(y_pred ==1 ):
         pred = "It is {0:.2f} safe to go ".format(y_pro_phishing*100)
-        return render_template('templates\index.html',xx =round(y_pro_non_phishing,2),url=url )
-    return render_template("templates\index.html", xx =-1)
+        return render_template('index.html',xx =round(y_pro_non_phishing,2),url=url )
+    return render_template('index.html', xx =-1)
 
 
 if __name__ == "__main__":
